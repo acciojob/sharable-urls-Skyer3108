5efdd0a981ee5c0d96c98854a8ab0f92d21a5b2c
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-04 17:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+
+const name=document.getElementById('name')
+const year=document.getElementById('year')
+
+const url=document.getElementById('url')
+
+const btn =document.getElementById('btn')
+
+btn.addEventListener('click',()=>{
+
+	url.textContent=`https://localhost:8080/${name.value}&${year.value}`
+
+	name.value=''
+	year.value=''
+
+	
+})

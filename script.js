64adedd756c48fe79ef74bbe9d0692639110f1f2
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-04 17:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ const year=document.getElementById('year')
 
 const url=document.getElementById('url')
 
-const btn =document.getElementById('btn')
+const btn =document.getElementById('button')
 
 btn.addEventListener('click',()=>{
 
-	url.textContent=`https://localhost:8080/${name.value}&${year.value}`
+	if(name.value&&year.value){
+		url.textContent=`https://localhost:8080/?name=${name.value}&year=${year.value}`
+	}
+
+	else if(name.value){
+		url.textContent=`https://localhost:8080/?name=${name.value}`
+		
+	}
+	else{
+		url.textContent=`https://localhost:8080/?${year.value}`
+	}
+	
 
 	name.value=''
 	year.value=''
